85bfc8071d8725224e31a6618f3b27b664adc98c
wyz <894272237@qq.com>
2020-12-15 14:51:14 +0800

    首页和三个跳转试试看

---
+++ app/src/main/java/com/example/mathtest/KSKSActivity.java
@@ -0,0 +1,14 @@
+package com.example.mathtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class KSKSActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ksks);
+
+    }
+}
