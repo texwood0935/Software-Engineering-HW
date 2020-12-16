1537cfa010a6b38ef59b4444122f6fd424cf71bd
wyz <894272237@qq.com>
2020-12-16 14:35:23 +0800

    version1.0 可以交差

--- app/src/main/java/com/example/mathtest/CTRJActivity.java
+++ app/src/main/java/com/example/mathtest/CTRJActivity.java
@@ -1,12 +1,29 @@
 package com.example.mathtest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class CTRJActivity extends BaseActivity {
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ctrj);
+
+        toolbar=findViewById(R.id.toolbar_ctrj);
+        toolbar.setNavigationIcon(R.mipmap.ic_keyboard_arrow_left_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                finish();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/mathtest/JZCTActivity.java
+++ app/src/main/java/com/example/mathtest/JZCTActivity.java
@@ -1,12 +1,29 @@
 package com.example.mathtest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class JZCTActivity extends BaseActivity {
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jzct);
+
+        toolbar=findViewById(R.id.toolbar_jzct);
+        toolbar.setNavigationIcon(R.mipmap.ic_keyboard_arrow_left_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                finish();
+            }
+        });
 
     }
 }
